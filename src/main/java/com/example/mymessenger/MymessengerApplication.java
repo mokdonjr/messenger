dfdd1a1541d6ea66a80bb76bc21fc905c4f7e12f
baegseungchan-gram <mokdonjr@gmail.com>
2020-03-08 19:48:57 +0900

    init spring boot project

---
+++ src/main/java/com/example/mymessenger/MymessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mymessenger/MymessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
