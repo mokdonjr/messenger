272c8441711baa408b2bc58bc9a389f18f237fdd
baegseungchan-gram <mokdonjr@gmail.com>
2020-03-18 05:02:34 +0900

    [빌드] jib 도커 entrypoint 스크립트 작성

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,8 @@
-java \
--cp \
-/app/resources/:/app/classes/:/app/libs/* \
-com.example.mymessenger.MyMessengerApplication
+#!/bin/sh
+
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -cp /app/resources/:/app/classes/:/app/libs/* "com.example.mymessenger.MyMessengerApplication"  "$@"
+
+#java \
+#-cp \
+#/app/resources/:/app/classes/:/app/libs/* \
+#com.example.mymessenger.MyMessengerApplication
