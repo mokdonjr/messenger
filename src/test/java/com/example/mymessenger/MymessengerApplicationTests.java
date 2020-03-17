e987dc78bc30ab7901566b3423bdf0fdc3e729e3
baegseungchan-gram <mokdonjr@gmail.com>
2020-03-18 04:30:13 +0900

    [빌드] jib 로 도커 이미지 빌드 및 푸쉬

--- src/test/java/com/example/mymessenger/MymessengerApplicationTests.java
+++ src/test/java/com/example/mymessenger/MymessengerApplicationTests.java
@@ -3,10 +3,28 @@ package com.example.mymessenger;
 import com.example.mymessenger.common.StaticBaseBeanTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.*;
+
 class MymessengerApplicationTests extends StaticBaseBeanTest {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException, ExecutionException, TimeoutException {
+		System.out.println("1");
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		Callable<String> task = this::doWork;
+
+		Future<String> future = executorService.submit(task);
+		System.out.println("future get : " + future.get(2, TimeUnit.SECONDS));
+		System.out.println("2");
+
+		//Executors.newSingleThreadExecutor().submit(this::doWork).get(2, TimeUnit.SECONDS);
+	}
+
+	public String doWork() throws InterruptedException {
+		System.out.println("do work start!");
+		TimeUnit.SECONDS.sleep(3);
+		System.out.println("do work end!");
+		return "do work return~";
 	}
 
 }
